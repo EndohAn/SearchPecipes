3b6d08ede4ea141b52953cf2f35430dcfa41cd6a
unknown <flowerrosa76@gmail.com>
2023-01-17 11:49:56 +0900

    first v

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import {useEffect,} from "react";
+
+
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ const MY_ID ="801c72eb";
+ const MY_KEY = "29c30b95fab850060525f868bb388667	— ";
+
+useEffect ( () => {
+    const getApi =async()=>{
+    const response = await fetch (`https://api.edamam.com/api/recipes/v2?type=public&q=tomato&app_id=${MY_ID}&app_key=${MY_KEY}`);
+    const data = await response.json();
+    console.log(data);
+
+  }
+  // getApi();
+},[])
+
+ 
+return(
+  <div>
+<h2>test</h2>
+  </div>
+)
+  
+  
 }
 
 export default App;
